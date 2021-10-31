84ae7feec0b0c06b1a912927916c50c301a0e3c1
Bùi Đức Anh Khoa <39853984+matuhn@users.noreply.github.com>
2021-10-31 19:42:13 +0700

    Add files via upload

--- check.py
+++ check.py
@@ -6,8 +6,10 @@ def check():
 	try:
 		resp = requests.get('http://chal8.web.letspentest.org/screen?url=file:///etc/passwd').json().get('status')
 	except:
+		print("crash app")
 		subprocess.run(['sudo', 'docker', 'restart', '3f1115475f19'])
 	if resp == "Timeout" or resp is None:
+		print("timeout")
 		subprocess.run(['sudo', 'docker', 'restart', '3f1115475f19'])
 
 schedule.every(3).minutes.do(check)
