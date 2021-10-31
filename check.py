7ee3cc42b9a446f2280a6278fbecff91f57b4351
Bùi Đức Anh Khoa <39853984+matuhn@users.noreply.github.com>
2021-10-31 19:44:04 +0700

    Add files via upload

--- check.py
+++ check.py
@@ -4,10 +4,11 @@ import time
 import subprocess
 def check():
 	try:
-		resp = requests.get('http://chal8.web.letspentest.org/screen?url=file:///etc/passwd').json().get('status')
+		resp = requests.get('http://chal8.web.letspentest.org/screen?url=file:///etc/passwd', verify=False, timeout=10).json().get('status')
 	except:
 		print("crash app")
 		subprocess.run(['sudo', 'docker', 'restart', '3f1115475f19'])
+	print(resp)
 	if resp == "Timeout" or resp is None:
 		print("timeout")
 		subprocess.run(['sudo', 'docker', 'restart', '3f1115475f19'])
